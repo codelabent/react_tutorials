55fe198fc56c73616046ef654dd2ea34f6bd3b7c
Marshallhch <marshallhch@gmail.com>
2026-01-20 19:36:51 +0900

    use ref

--- react_hooks/src/App.jsx
+++ react_hooks/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import UseRef from './components/UseRef';
 
 const App = () => {
   return (
-    <>
-      <h2 className="text-3xl font-bold underline">React Hooks</h2>
-    </>
+    <div>
+      <UseRef />
+    </div>
   );
 };
 
